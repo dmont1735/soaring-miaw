2e82682de5879552afebf7f902d1898c6324faae
dmont1735 <48958298+dmont1735@users.noreply.github.com>
2025-12-21 10:37:26 +0000

    Added art
    Added 2D sprites and implemented simple animation

--- scripts/ObstacleSpawner.gd
+++ scripts/ObstacleSpawner.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var obstacle_scenes : Array[PackedScene]
 @export var move_speed := 250.0
-@export var lower_start_y := 400.0
+@export var lower_start_y := 730.0
 @export var higher_start_y := 0.0
 @onready var obstacle_container := $"../ObstacleContainer"
 var speed_ramp := 1.0
@@ -16,7 +16,7 @@ func spawn_obstacle():
 
 	var scene = obstacle_scenes.pick_random()
 	var obstacle = scene.instantiate()
-	var spawning_position= Vector2(global_position.x, lower_start_y)
+	var spawning_position = Vector2(global_position.x, lower_start_y)
 	
 	obstacle.set_meta("move_speed", move_speed * speed_ramp)
 	obstacle.position = spawning_position
--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var jump_speed := -800.0
 @export var gravity := 1800.0
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 signal player_died()
 
 var is_alive := true
@@ -9,7 +11,8 @@ var is_alive := true
 func _ready():
 	GameManager.player = self
 	player_died.connect(GameManager._on_player_died)
-	
+	animated_sprite_2d.play("fall")
+		
 func _physics_process(delta):
 	if not is_alive:
 		return
@@ -19,10 +22,22 @@ func _physics_process(delta):
 	# Jump (space or touch)
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = jump_speed
+		animated_sprite_2d.play("boost")
 
 	# Move player
 	move_and_slide()
 
+func update_animation():
+	if not is_alive:
+		return
+
+	if velocity.y < 0:
+		if animated_sprite_2d.animation != "boost":
+			animated_sprite_2d.play("boost")
+	else:
+		if animated_sprite_2d.animation != "fall":
+			animated_sprite_2d.play("fall")
+			
 func die():
 	is_alive = false
 	player_died.emit()
